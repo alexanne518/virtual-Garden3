f2376352247b9513cd7e3fa246d4821dcf3efc72
Sofia Guerra Quintero <sofiaguerraquintero@gmail.com>
2025-03-13 15:21:40 -0400

    commit 1 - base code...

--- src/main/java/com/example/virtualgarden3/model/Plant.java
+++ src/main/java/com/example/virtualgarden3/model/Plant.java
@@ -2,5 +2,14 @@ package com.example.virtualgarden3.model;
 
 public class Plant {
 
-}
+    private String name;
+    private int growingRatio;
+    private Soil soil;
 
+    public Plant(String name, int growingRatio, Soil soil) {
+        this.name = name;
+        this.growingRatio = growingRatio;
+        this.soil = soil;
+    }
+
+}
