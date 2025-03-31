25d8e25dfd5d4a534da445e59dde4d4ffe40fc59
alexanne <alexannefortin18@gmail.com>
2025-03-31 17:22:09 -0400

    the base of the our app

--- src/main/java/com/example/virtualgarden3/model/AfricanViolet.java
+++ src/main/java/com/example/virtualgarden3/model/AfricanViolet.java
@@ -1,5 +1,26 @@
 package com.example.virtualgarden3.model;
 
-public class AfricanViolet{
+import java.util.Date;
 
+public class AfricanViolet extends Plant{
+    private String soilType; //idk if this is gonna be a class eathereee
+    private int waterAmount;
+
+    public AfricanViolet(int plantId, String name, int waterLevel, String soilType, int waterAmount) {
+        super(plantId, name, waterLevel); //using the parent class
+        this.soilType = SoilType.LIGHT_SOIL.getDisplayName();  // Set to the specific soil type
+        this.waterAmount = waterAmount;
+    }
+
+    @Override
+    public void waterPlant() {
+        waterLevel += waterAmount; // Ferns require a lot of water
+        if (waterLevel > 100) {
+             System.out.println("the plant is already at 100% do you want to continue?");
+             //if answer is yes water if no dont
+             //gonna get the answer form a form so...
+             this.lastWatered = new Date(); //updating to now
+        }
+        System.out.println(name + " watered. Current water level: " + waterLevel + "%.");
+    }
 }
--- src/main/java/com/example/virtualgarden3/model/Main.java
+++ src/main/java/com/example/virtualgarden3/model/Main.java
@@ -1,7 +1,12 @@
 package com.example.virtualgarden3.model;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("what is upp bro!");
+    public static void main(String[] args)
+    {
+        UserRegistration newUser = new UserRegistration(1, "alex", "123");
+
+        System.out.println(newUser.toString());
+
+        Plant newplant = new Plant();
     }
 }
